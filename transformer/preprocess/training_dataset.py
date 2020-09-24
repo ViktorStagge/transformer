d85e9fd1fcd38ecc1f352e97185ca9302a8c69b9
Viktor Stagge <viktorstagge@gmail.com>
2020-09-24 21:21:03 +0200

    fix: move most of dataset generation logic to generator due to memory usage

--- transformer/preprocess/training_dataset.py
+++ transformer/preprocess/training_dataset.py
@@ -3,77 +3,55 @@ import numpy as np
 import compress_pickle
 
 from transformer.utils import get_tqdm
+from transformer.utils import get_logger
+
+logger = get_logger('transformer')
 
 
 def create_training_dataset(english_tokens,
                             german_tokens,
                             max_samples=None,
                             sample_length=100,
-                            save_dataset=False,
-                            save_interval=100000,
+                            save_dataset=True,
+                            save_interval=1000,
                             save_dir=None,
                             save_compression='zipfile',
                             tqdm=None):
     max_samples = min(max_samples or len(english_tokens), len(english_tokens), len(german_tokens))
+    # save_interval = min(save_interval, len(english_tokens), len(german_tokens))
+
     if tqdm is None:
         tqdm = get_tqdm()
-    x = []
-    y = []
-    y_target_position_info = []
 
-    for sample_index, e_sample, d_sample in zip(tqdm(range(max_samples), desc='create_training_data'),
-                                                english_tokens,
-                                                german_tokens):
-        tokens_in_sample = min(len(e_sample), len(d_sample), sample_length)
-
-        e_sample_entries = _preprocess_create_training_data_samples(e_sample, sample_length, tokens_in_sample)
-        d_sample_entries = _preprocess_create_training_data_samples(d_sample, sample_length, tokens_in_sample)
-        y_sample_target_position = np.arange(0, tokens_in_sample)
-
-        x.append(e_sample_entries)
-        y.append(d_sample_entries)
-        y_target_position_info.append(y_sample_target_position)
-
-        if sample_index % save_interval == 0 and save_dataset:
-            os.makedirs(save_dir, exist_ok=True)
-
-            x = np.concatenate(x)
-            y = np.concatenate(y)
-            y_target_position_info = np.concatenate(y_target_position_info)
-            assert len(x) == len(y) == len(y_target_position_info), \
-                f'unexpected data mismatch for x={len(x)}, y={len(y)}, y_target={len(y_target_position_info)}'
-
-            dataset = (x, y, y_target_position_info)
-            compress_pickle.dump(dataset,
-                                 path=os.path.join(save_dir, f'train_{sample_index}'),
-                                 compression=save_compression)
-            x = []
-            y = []
-            y_target_position_info = []
+    assert len(english_tokens) == len(german_tokens), \
+        f'unexpected data mismatch for english={len(english_tokens)}, german={len(german_tokens)}'
 
     if save_dataset:
-        return
+        os.makedirs(save_dir, exist_ok=True)
 
-    x = np.concatenate(x)
-    y = np.concatenate(y)
-    y_target_position_info = np.concatenate(y_target_position_info)
+        logger.debug(f'creating dataset: i={0}, max_samples={max_samples}, step={save_interval}')
+        for i in tqdm(range(0, max_samples, save_interval), desc='create_training_data'):
+            logger.debug(f'creating dataset: i={i}, max_samples={max_samples}, step={save_interval}')
+            assert isinstance(english_tokens, list), \
+                f'unexpected format received: received={type(english_tokens)}, expected=<list>'
 
-    assert len(x) == len(y) == len(y_target_position_info), \
-        f'unexpected data mismatch for x={len(x)}, y={len(y)}, y_target={len(y_target_position_info)}'
+            en = english_tokens[i: i+save_interval]
+            en = [row[:sample_length] for row in en]
 
-    return x, y, y_target_position_info
+            de = german_tokens[i: i+save_interval]
+            de = [row[:sample_length] for row in de]
 
+            assert len(en) == len(de), \
+                f'unexpected data mismatch for english={len(en)}, german={len(de)}'
 
-def _preprocess_create_training_data_samples(sample,
-                                             sample_length,
-                                             tokens_in_sample):
-    if sample_length < len(sample):
-        sample = sample[:sample_length]
+            dataset = (en, de)
+            compress_pickle.dump(dataset,
+                                 path=os.path.join(save_dir, f'train_{i}'),
+                                 compression=save_compression)
 
-    sample_entries = np.tile(sample, reps=(tokens_in_sample, 1))
-    sample_entries = np.tril(sample_entries)
+    if not save_dataset:
+        raise NotImplementedError('Down-prioritized due to too typically high memory requirements.')
+        assert len(en) == len(de), \
+            f'unexpected data mismatch for english={len(en)}, german={len(de)}'
 
-    padding_length = sample_length - len(sample)
-    if padding_length:
-        sample_entries = np.pad(sample_entries, ((0, 0), (0, padding_length)), mode='constant', constant_values=0)
-    return sample_entries
+        return en, de
--- transformer/train/generators.py
+++ transformer/train/generators.py
@@ -5,6 +5,9 @@ import compress_pickle
 from keras.utils import to_categorical
 
 from transformer.utils.utils import get_tqdm
+from transformer.utils.utils import get_logger
+
+logger = get_logger('transformer')
 
 
 class NextTokenBatchGenerator:
@@ -14,9 +17,10 @@ class NextTokenBatchGenerator:
                  data_dir,
                  epochs=None,
                  epoch_steps=None,
-                 shuffle=False,
                  batch_size,
                  vocab_size,
+                 sample_length=100,
+                 shuffle=False,
                  tqdm=None):
         """Creates a generator which returns a batch of data, suitable for using with keras.Model.fit_generator()
         """
@@ -24,14 +28,14 @@ class NextTokenBatchGenerator:
             f'specified directory for files does not exist: {data_dir}'
         if epochs is None:
             epochs = int(1e16)
-        tqdm = get_tqdm(tqdm)
-        _epoch_steps = epoch_steps
+        self.tqdm = get_tqdm(tqdm)
+        self.epoch_steps_specified = epoch_steps
+        self.sample_length = sample_length
 
         self.data_paths = [os.path.join(data_dir, filename) for filename in os.listdir(data_dir)]
-        self.total_sample_length = sum(len(compress_pickle.load(path)[0]) for path in tqdm(self.data_paths,
-                                                                                           desc='counting samples'))
+        self.total_sample_length = self._count_total_sample_length()
 
-        epoch_steps = min(self.total_sample_length, _epoch_steps or self.total_sample_length)
+        epoch_steps = min(self.total_sample_length, self.epoch_steps_specified or self.total_sample_length)
         epoch_steps = epoch_steps - batch_size - epoch_steps % batch_size
 
         self.data = None
@@ -39,11 +43,9 @@ class NextTokenBatchGenerator:
         self.vocab_size = vocab_size
         self.epochs = epochs
         self.epoch_steps = epoch_steps
-        self.epoch_steps_specified = _epoch_steps
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.steps_per_epoch = epoch_steps // batch_size
-        self.tqdm = tqdm
 
     def __call__(self):
         def _next_token_batch_generator():
@@ -53,20 +55,36 @@ class NextTokenBatchGenerator:
 
                 epoch_step = 0
                 for path in self.data_paths:
-                    x_data, y_data, y_target_position_info = compress_pickle.load(path)
-                    end_step = min(self.epoch_steps - epoch_step,
-                                   len(x_data) - len(x_data) % self.batch_size)
+                    logger.debug(f'{path}')
+                    english_samples, german_samples = compress_pickle.load(path)
+                    logger.debug(f'en={len(english_samples)}, de={len(german_samples)}')
+
+                    x_input, y, y_position = self._expand_batch(english_samples,
+                                                                german_samples,
+                                                                sample_length=self.sample_length)
+                    logger.debug(f'x_input={len(x_input)}, y={len(y)}, y_position={len(y_position)}')
+
+                    y_target = np.array(list(row[col] for row, col in zip(y, y_position)))
+
+                    for i, pos in enumerate(y_position):
+                        y[i, pos] = 0.
+                    # y[list(range(len(y))), y_position] = 0.
+                    x_output = y
+                    logger.debug(f'x_output={len(x_output)}, y_target={len(y_target)}')
+
+                    end_step = sum(max(len(e), len(d)) for e, d in zip(english_samples, german_samples))
+                    end_step -= end_step % self.batch_size
+                    end_step = min(end_step, self.epoch_steps - epoch_step)
+                    logger.debug(f'end_step={end_step}')
 
                     for i in range(0, end_step, self.batch_size):
-                        x_batch_input = x_data[i: i+self.batch_size]
-                        x_batch_output = y_data[i: i+self.batch_size]
-                        target_position = y_target_position_info[i: i+self.batch_size]
+                        logger.debug(f' about to yield batch')
+                        x_input_batch = x_input[i: i+self.batch_size]
+                        x_output_batch = x_output[i: i+self.batch_size]
+                        y_target_batch = y_target[i: i+self.batch_size]
 
-                        y_batch = np.array(list(row[col] for row, col in zip(x_batch_output, target_position)))
-                        x_batch_output[:, target_position] = 0.
-
-                        x_batch = [x_batch_input, x_batch_output]
-                        y_batch = to_categorical(y_batch, num_classes=self.vocab_size)
+                        x_batch = [x_input_batch, x_output_batch]
+                        y_batch = to_categorical(y_target_batch, num_classes=self.vocab_size)
 
                         epoch_step += self.batch_size
                         yield x_batch, y_batch
@@ -76,3 +94,52 @@ class NextTokenBatchGenerator:
 
     def new(self):
         return self()
+
+    @staticmethod
+    def _expand_batch(x_batch, y_batch, sample_length):
+        x = []
+        y = []
+        y_position = []
+
+        for sample_index, (e_sample, d_sample) in enumerate(zip(x_batch, y_batch)):
+            tokens_in_sample = min(max(len(e_sample), len(d_sample)), sample_length)
+
+            e_i = NextTokenBatchGenerator._expand_sample(e_sample, sample_length, tokens_in_sample)
+            d_i = NextTokenBatchGenerator._expand_sample(d_sample, sample_length, tokens_in_sample)
+            y_position_i = np.arange(0, tokens_in_sample)
+
+            x.append(e_i)
+            y.append(d_i)
+            y_position.append(y_position_i)
+
+        x = np.concatenate(x)
+        y = np.concatenate(y)
+        y_position = np.concatenate(y_position)
+
+        return x, y, y_position
+
+    @staticmethod
+    def _expand_sample(sample,
+                       sample_length,
+                       tokens_in_sample):
+        if sample_length < len(sample):
+            sample = sample[:sample_length]
+
+        sample_entries = np.tile(sample, reps=(tokens_in_sample, 1))
+        sample_entries = np.tril(sample_entries)
+
+        padding_length = sample_length - len(sample)
+        if padding_length:
+            sample_entries = np.pad(sample_entries, ((0, 0), (0, padding_length)), mode='constant', constant_values=0)
+        return sample_entries
+
+    def _count_total_sample_length(self):
+        total_sample_length = 0
+
+        for path in self.tqdm(self.data_paths, desc='counting samples'):
+            en, de = compress_pickle.load(path)
+            for en_sample, de_sample in zip(en, de):
+                tokens_in_sample = min(max(len(en_sample), len(de_sample)), self.sample_length)
+                total_sample_length += tokens_in_sample
+
+        return total_sample_length
